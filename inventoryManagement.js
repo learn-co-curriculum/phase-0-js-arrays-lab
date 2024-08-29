7e7bfc4a72493f2401e2746a4aebba0dbcd0d6d1
Nicholas Christensen <nickachristensen@gmail.com>
2024-08-29 12:36:42 -0400

    Update inventoryManagement.js

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -6,6 +6,11 @@ function logFirstProduct() {
     console.log(products[0]); // Logs "Laptop"
 }
 
+// Add a Product
+function addProduct(productName) {
+    products.push(productName);
+}
+
 // Update Product Information
 function updateProductName(index, newName) {
     products[index] = newName;
@@ -16,11 +21,11 @@ function removeLastProduct() {
     products.pop();
 }
 
-
-// Export the necessary parts for testing. Don't modify.
+// Export the necessary parts for testing
 module.exports = {
-  products,
-  logFirstProduct,
-  updateProductName,
-  removeLastProduct
+  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
+  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
+  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
+  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
+  products
 };
